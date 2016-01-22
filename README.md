a4664e49688b4e93c626f6de22e92440fcdbccb7
Daniel Janus <dj@danieljanus.pl>
2016-01-22 13:58:04 +0100

    Add clj-nkjp.disamb.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 _English:_ Clojure utilities to process the [National Corpus of
 Polish][1] (NKJP) data.
 
-_Polish:_ Narzędzia w Clojure do przetwarzania danych 
+_Polish:_ Narzędzia w Clojure do przetwarzania danych
 [Narodowego Korpusu Języka Polskiego][1].
 
  [1]: http://nkjp.pl/
@@ -16,8 +16,15 @@ Na ten projekt składają się następujące przestrzenie nazw:
 
    Wyniki konwersji (3889 plików .clj skompresowanych programem
    7zip; 10 MB) można pobrać [stąd][2].
-   
+
  [2]: http://danieljanus.pl/nkjp-clj.7z
+
+ * _clj-nkjp.disamb_: Generuje z plików .clj mapę przyporządkowującą
+   każdemu typowi leksemu w milionowym podkorpusie NKJP jego najczęstszą
+   formę podstawową (dane do lematyzatora unigramowego; por.
+   projekt [Polelum][3]).
+
+ [3]: http://github.com/nathell/polelum
 
 ## License / Licencja
 
---
+++ src/clj_nkjp/disamb.clj
@@ -0,0 +1,40 @@
+(ns clj-nkjp.disamb
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]))
+
+(defn streamline
+  [tree]
+  (filter #(and (map? %) (= (:type %) "morph")) (tree-seq vector? seq tree)))
+
+(defn orth-disamb-pairs
+  [morphs]
+  (for [morph morphs]
+    {(first (:orth morph))
+     {(-> morph :disamb first :interpretation first (string/split #":") first) 1}}))
+
+(defn merge-freqs
+  [freq-maps]
+  (apply merge-with (partial merge-with +) freq-maps))
+
+(defn file-freqs
+  [f]
+  (-> f slurp read-string streamline orth-disamb-pairs merge-freqs))
+
+(def input-dir "/home/nathell/corpora/nkjp-clj")
+
+(defn grand-freq-map
+  []
+  (let [files (filter #(.endsWith (str %) ".clj") (file-seq (io/file input-dir)))]
+    (merge-freqs (map file-freqs files))))
+
+(defn disamb-map
+  [m]
+  (into {}
+        (for [[k v] m]
+          [k (key (first (sort-by val > v)))])))
+
+(defn generate
+  [out]
+  (with-open [f (io/writer out)]
+    (binding [*out* f]
+      (prn (disamb-map (grand-freq-map))))))
